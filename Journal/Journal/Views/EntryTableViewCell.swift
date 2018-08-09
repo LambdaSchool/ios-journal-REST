1611ae49dc257bb96305a5c669de71a61cd5bb51
Jeremy Taylor <jtaylor@jctaylor.org>
2018-08-09 11:45:41 -0500

    Storyboard setup done

---
+++ Journal/Journal/Views/EntryTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Jeremy Taylor on 8/9/18.
+//  Copyright © 2018 Bytes-Random L.L.C. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    
+}
