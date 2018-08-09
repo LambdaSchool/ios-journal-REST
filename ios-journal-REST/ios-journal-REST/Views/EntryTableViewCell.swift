68fabc0bdedfd251c0ce00314219207a0b18af29
Lambda-School-Loaner-11 <lambda-school-loaner-11@Lambda-School-Loaner-11s-MacBook-Pro.local>
2018-08-09 09:49:08 -0700

    set up Firebase and storyboards

---
+++ ios-journal-REST/ios-journal-REST/Views/EntryTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  EntryTableViewCell.swift
+//  ios-journal-REST
+//
+//  Created by Lambda-School-Loaner-11 on 8/9/18.
+//  Copyright © 2018 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var timestamp: UILabel!
+    
+    @IBOutlet weak var bodyText: UILabel!
+    
+    
+}
