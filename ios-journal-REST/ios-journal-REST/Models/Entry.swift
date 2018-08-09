cccc4eb2214381ca67cea632f1be55a532e6d502
Lambda-School-Loaner-11 <lambda-school-loaner-11@Lambda-School-Loaner-11s-MacBook-Pro.local>
2018-08-09 10:33:45 -0700

    set up struct

---
+++ ios-journal-REST/ios-journal-REST/Models/Entry.swift
@@ -0,0 +1,24 @@
+//
+//  Entry.swift
+//  ios-journal-REST
+//
+//  Created by Lambda-School-Loaner-11 on 8/9/18.
+//  Copyright © 2018 David Doswell. All rights reserved.
+//
+
+import Foundation
+
+struct Entry: Equatable, Codable {
+    var title: String
+    var bodyText: String
+    var timestamp: Date
+    let identifier: String
+    
+    init(title: String, bodyText: String, timestamp: Date, identifier: String) {
+        
+        self.title = title
+        self.bodyText = bodyText
+        self.timestamp = Date()
+        self.identifier = UUID().uuidString
+    }
+}
