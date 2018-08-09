5b3a2896ed32274f50508382546637fec75aed10
Jeremy Taylor <jtaylor@jctaylor.org>
2018-08-09 13:15:31 -0500

    Finish EntryController code

--- Journal/Journal/View Controllers/EntryDetailViewController.swift
+++ Journal/Journal/View Controllers/EntryDetailViewController.swift
@@ -13,16 +13,12 @@ class EntryDetailViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var textView: UITextView!
     
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func save(_ sender: UIBarButtonItem) {
