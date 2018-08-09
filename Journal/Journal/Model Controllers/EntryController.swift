40c41a6e1cb72eb9c929253821b6b30dff84cb51
Linh Bouniol <paulalinh89@gmail.com>
2018-08-09 13:33:32 -0700

    Created model object and model controller to hold GET, PUT, POST data

---
+++ Journal/Journal/Model Controllers/EntryController.swift
@@ -0,0 +1,109 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by Linh Bouniol on 8/9/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import Foundation
+
+class EntryController {
+    
+    private enum HTTPMethod: String {
+        case get = "GET"
+        case put = "PUT"
+        case post = "POST"
+        case delete = "DELETE"
+    }
+    
+    var entries: [Entry] = []
+    
+    static let baseURL = URL(string: "https://journal-c0120.firebaseio.com/")!
+    
+    // PUT an existing entry
+    func put(entry: Entry, completion: @escaping (Error?) -> Void) {
+        
+        // How to know if you need to initialize a new Entry object, and append the encoded entry to the Entry variable?
+        
+        let url = EntryController.baseURL.appendingPathComponent(entry.identifier).appendingPathExtension("json")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.put.rawValue
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(entry)
+        } catch {
+            NSLog("Unable to encode \(entry): \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error saving entry to server: \(error)")
+                DispatchQueue.main.async {
+                    completion(error)
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                completion(nil)
+            }
+        }.resume()
+    }
+    
+    // Create a new entry using the PUT method above
+    func createEntry(withTitle title: String, bodyText: String, completion: @escaping (Error?) -> Void) {
+        let entry = Entry(title: title, bodyText: bodyText)
+        
+        // Pass in completion of createEntry() into the completion closure of pull() - this will forward the completion of put() to the caller of createEntry() so the error can be handled there.
+        
+        put(entry: entry, completion: completion)
+    }
+    
+    // Fetch the data using GET
+    func fetchEntries(completion: @escaping (Error?) -> Void) {
+        let url = EntryController.baseURL.appendingPathExtension("json")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error saving entry to server: \(error)")
+                DispatchQueue.main.async {
+                    completion(error)
+                }
+                return
+            }
+            
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(error)
+                }
+                return
+            }
+            
+            do {
+                let decodedEntries = try JSONDecoder().decode([String: Entry].self, from: data)
+                let sortedDecodedEntries = decodedEntries.map { $0.value }.sorted { $0.timestamp < $1.timestamp }
+                
+                DispatchQueue.main.async {
+                    self.entries = sortedDecodedEntries
+                }
+            } catch {
+                NSLog("Error decoding received data: \(error)")
+                DispatchQueue.main.async {
+                    completion(error)
+                }
+                return
+            }
+        }
+    }
+ }
+
+
+
+
+
--- Journal/Journal/View Controllers/EntriesTableViewController.swift
+++ Journal/Journal/View Controllers/EntriesTableViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 class EntriesTableViewController: UITableViewController {
+    
+    let entryController = EntryController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        // Mock entry to show on the database for testing
+        entryController.createEntry(withTitle: "TEst5", bodyText: "messagebroken") { (error) in
+            if let error = error {
+                NSLog("Error creating entry: \(error)")
+                return
+            }
+            
+            self.tableView.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
