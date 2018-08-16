f03c942aed7b55423dad61bcfcd8431d10335830
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-16 12:55:16 -0400

    Set up storyboards

---
+++ Journal/Journal/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Lisa Sampson on 8/16/18.
+//  Copyright © 2018 Lisa Sampson. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveEntryButtonTapped(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBOutlet weak var entryTitleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+}
