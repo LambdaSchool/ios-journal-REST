6586eb98aff3188cd6c6a8626a4ff948dd46ec66
Daniela Parra <dyparra47@gmail.com>
2018-09-13 16:18:00 -0400

    Finish PUT entry functionality in model controller.

--- JournalREST/JournalREST/EntryDetailViewController.swift
+++ JournalREST/JournalREST/EntryDetailViewController.swift
@@ -9,31 +9,10 @@
 import UIKit
 
 class EntryDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     
     @IBAction func saveEntry(_ sender: Any) {
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var bodyTextView: UITextView!
