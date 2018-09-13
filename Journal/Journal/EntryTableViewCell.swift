863bbfda39a94b7c739dd6bcf5dfb0de4afb9845
Moin Uddin <U_Moin75@yahoo.com>
2018-09-13 17:43:04 -0400

    Halfway through setting up StoryBoards

---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Moin Uddin on 9/13/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
