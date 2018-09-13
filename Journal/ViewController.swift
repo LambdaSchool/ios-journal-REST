c02b87c6c58a62001287c9ceddadbfb60ec18af9
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2018-09-13 17:11:07 -0500

    Initial commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Jason Modisett on 9/13/18.
+//  Copyright © 2018 Jason Modisett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
