d4b50e1619eaba86ddfc57ca344a660993e944bc
Daniela Parra <dyparra47@gmail.com>
2018-09-13 17:14:37 -0400

    Finish fetch entries functionality.

--- JournalREST/JournalREST/EntryController.swift
+++ JournalREST/JournalREST/EntryController.swift
@@ -10,11 +10,65 @@ import Foundation
 
 class EntryController {
     
+    // MARK: - CRUD
+    
     func createEntry(title: String, bodyText: String, completion: @escaping (Error?) -> Void) {
         let entry = Entry(title: title, bodyText: bodyText)
         
-        //Looks good?
         putEntry(entry: entry, completion: completion)
+    }
+    
+    func updateEntry(entry: Entry, title: String, bodyText: String, completion: @escaping (Error?) -> Void) {
+        guard let index = entries.index(of: entry) else { return }
+        
+        let updatedEntry = Entry(title: title, bodyText: bodyText, timestamp: Date(), identifier: entry.identifier)
+        entries.remove(at: index)
+        entries.insert(updatedEntry, at: index)
+        
+        putEntry(entry: updatedEntry, completion: completion)
+    }
+    
+    // MARK: - Networking
+    
+    func fetchEntries(completion: @escaping (Error?) -> Void) {
+        
+        let requestURL = baseURL.appendingPathExtension("json")
+        
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching data: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from data task.")
+                completion(NSError())
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            do {
+                let entryResults = try jsonDecoder.decode([String : Entry].self , from: data)
+                
+                let resultValues = entryResults.map( { $0.value } )
+                let sortedKeys = resultValues.sorted(by: { (entry1, entry2) -> Bool in
+                    return entry1.identifier > entry2.identifier
+                })
+               
+                self.entries = sortedKeys
+                print("Fetch successful")
+                completion(nil)
+                
+            } catch {
+                NSLog("Error decoding data: \(error)")
+                completion(error)
+                return
+            }
+            
+        }.resume()
     }
     
     func putEntry(entry: Entry, completion: @escaping (Error?) -> Void) {
