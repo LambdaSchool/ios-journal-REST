2355500b18a1c7c67a2a83e04ea203fef5536cc4
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-20 20:46:45 -0500

    func put() completed

---
+++ JournalREST/JournalREST/Model Controlle/EntryController.swift
@@ -0,0 +1,62 @@
+//
+//  EntryController.swift
+//  JournalREST
+//
+//  Created by Ilgar Ilyasov on 9/20/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class EntryController {
+    
+    // MARK: - Properties
+    
+    var etries: [Entry] = []
+    
+    
+    // MARK: - Base URL
+    
+    static let baseURl = URL(string: "https://journal-b5918.firebaseio.com/")!
+    
+    
+    // MARK: Put functions
+    
+    func put(entry: Entry, completion: @escaping (Error?) -> Void) {
+        
+        // Create a request url
+        var url = EntryController.baseURl.appendingPathComponent(entry.identifier)
+        url.appendPathExtension("json")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.put.rawValue
+        
+        // Encode data
+        let encoder = JSONEncoder()
+        
+        do {
+            let encodedEntry = try encoder.encode(entry)
+            request.httpBody = encodedEntry
+            completion(nil)
+        } catch {
+            NSLog("Error encoding \(entry): \(error)")
+            completion(error)
+            return
+        }
+        
+        // Create a task
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            
+            // Handle errors
+            if let error = error {
+                NSLog("Error transfering data: \(error)")
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
+        
+    }
+    
+}
