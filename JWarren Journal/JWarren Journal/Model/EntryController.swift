5f4b8650fe66be6a8b4f9ec64d0ef4933fceb745
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-20 19:36:51 -0700

    set up Entry Controller with Base URL

---
+++ JWarren Journal/JWarren Journal/Model/EntryController.swift
@@ -0,0 +1,17 @@
+//
+//  EntryController.swift
+//  JWarren Journal
+//
+//  Created by Welinkton on 9/20/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import Foundation
+
+class EntryController {
+    
+    var entries: [Entry] = []
+    
+    let baseURL = URL(string: "https://jwarren-journal.firebaseio.com/")
+    
+}
