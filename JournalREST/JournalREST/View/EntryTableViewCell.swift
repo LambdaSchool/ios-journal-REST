27bc82a923a06d225b0c893951c7047b8a5dae07
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-20 19:20:58 -0500

    Storyboard set up completed

---
+++ JournalREST/JournalREST/View/EntryTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  EntryTableViewCell.swift
+//  JournalREST
+//
+//  Created by Ilgar Ilyasov on 9/20/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+}
