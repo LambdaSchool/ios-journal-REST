3ed440fae0165f9e4cc7fc5347b6aa757357e034
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-20 19:41:36 -0500

    EntryDetailViewController

---
+++ JournalREST/JournalREST/View Controller/EntryDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  EntryDetailViewController.swift
+//  JournalREST
+//
+//  Created by Ilgar Ilyasov on 9/20/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func saveBarButtonTapped(_ sender: Any) {
+    }
+    
+}
