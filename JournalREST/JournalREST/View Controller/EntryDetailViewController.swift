f16a289826c2aef8d00f22cba393afb4cc76a21f
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-20 23:14:57 -0500

    func saveBarButtonTapped() completed. EntryDetailViewController's updateViews() function completed

--- JournalREST/JournalREST/View Controller/EntryDetailViewController.swift
+++ JournalREST/JournalREST/View Controller/EntryDetailViewController.swift
@@ -12,8 +12,10 @@ class EntryDetailViewController: UIViewController {
     
     // MARK: - Properties
     
-    var entry: Entry?
     var entryController: EntryController?
+    var entry: Entry? {
+        didSet { updateViews() }
+    }
     
     
     // MARK: - Outlets
@@ -26,13 +28,50 @@ class EntryDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateViews()
     }
     
     
     // MARK: - Actions
     
     @IBAction func saveBarButtonTapped(_ sender: Any) {
+        guard let title = titleTextField.text,
+            let body = bodyTextView.text else { return }
+        
+        // If entry is not nil
+        if let entry = entry {
+            entryController?.updateEntry(entry: entry, title: title, bodyText: body, completion: { (_) in
+                DispatchQueue.main.async {
+                    self.navigationController?.popViewController(animated: true)
+                }
+            })
+        } else {
+            entryController?.createEntry(title: title, bodyText: body, completion: { (_) in
+                DispatchQueue.main.async {
+                    self.navigationController?.popViewController(animated: true)
+                }
+            })
+        }
+        
+    }
+    
+    // MARK: - Update view
+    
+    private func updateViews() {
+        
+        if isViewLoaded {
+            
+            // If entry is not nil then set views values
+            if let entry = entry {
+                title = entry.title
+                titleTextField.text = entry.title
+                bodyTextView.text = entry.bodyText
+            }
+            // If it's nil
+            else {
+                title = "Create Entry"
+            }
+        }
     }
     
 }
