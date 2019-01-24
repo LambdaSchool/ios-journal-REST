bd56b9120d58711929fac2bfd8caed7acb825a5a
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-24 14:03:44 -0800

    Connect data with cell.

--- Journal/Journal/EntryTableViewCell.swift
+++ Journal/Journal/EntryTableViewCell.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class EntryTableViewCell: UITableViewCell {
     
-    
+    func updateViews() {
+        guard let entry = entry else { return }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        let date = entry.timestamp
+        let dateString = dateFormatter.string(from: date)
+        
+        title.text = entry.title
+        time.text = dateString
+        bodyText.text = entry.bodyText
+    }
     
     // Properties
+    
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
 
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var time: UILabel!
