8a61ca4f7c9fe41ab1c6f05a3bc9b030bc02d0c1
Angel <angelbuenrostro@outlook.com>
2019-01-24 18:47:08 -0500

    Add storyboards and custom classes

---
+++ Journal/Journal/Views/EntryTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Angel Buenrostro on 1/24/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var entryLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
