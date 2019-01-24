3f161b5a5c215980450dc2815e87c9633f60a863
Frulwinn <frulwinn@gmail.com>
2019-01-24 13:14:39 -0800

    created Entry struct

---
+++ Journal/Journal/Entry.swift
@@ -0,0 +1,17 @@
+//  Copyright © 2019 Frulwinn. All rights reserved.
+
+import Foundation
+
+struct Entry: Equatable, Codable {
+    var title: String
+    var bodyText: String
+    var timestamp: Date
+    var identifier: String
+    
+    init(title: String, bodyText: String, timestamp: Date = Date(), identifier: String) {
+        self.title = title
+        self.bodyText = bodyText
+        self.timestamp = timestamp
+        self.identifier = identifier
+    }
+}
