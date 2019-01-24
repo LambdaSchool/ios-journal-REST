0240b2279dcac92637a52c38c451d360aee653f0
Frulwinn <frulwinn@gmail.com>
2019-01-24 13:54:53 -0800

    added url request and do catch block on func put

--- Journal/Journal/EntryController.swift
+++ Journal/Journal/EntryController.swift
@@ -7,4 +7,22 @@ let baseURL = URL(string: "https://books-118d9.firebaseio.com/")!
 class EntryController {
     var entries: [Entry] = []
     
+    func put(entry: String, completion: @escaping(Error?) -> Void) {
+        let entry = Entry(title: title, bodyText: bodyText, timestamp: Date, identifier: identifier)
+        let url = baseURL.appendingPathComponent(entry.identifier)
+        let newURL = url.appendingPathExtension("json")
+        
+        var request = URLRequest(url: newURL)
+        request.httpMethod = "PUT"
+        
+        do {
+            let encoder = JSONEncoder()
+            request.httpBody = try encoder.encode(entry)
+        } catch {
+            print(error)
+            completion(error)
+            return
+        }
+    }
+    
 }
