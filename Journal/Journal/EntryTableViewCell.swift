c3d0cd8cc697f12819562b34316229d5b1fc18e4
Frulwinn <frulwinn@gmail.com>
2019-01-24 12:58:09 -0800

    added 3 labels for tableview cell and hooked the outlets

--- Journal/Journal/EntryTableViewCell.swift
+++ Journal/Journal/EntryTableViewCell.swift
@@ -4,6 +4,11 @@ import UIKit
 
 class EntryTableViewCell: UITableViewCell {
 
-   
-
+    //MARK: Properties
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var timestampLabel: UILabel!
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    
 }
