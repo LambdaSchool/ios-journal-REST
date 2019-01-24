c6d5594f8d1f237db62a34f2c9eb93918d8e8056
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-24 12:50:48 -0800

    Setup storyboard with connections to view controllers and custom cell vc.

---
+++ Journal/Journal/EntryDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Nathanael Youngren on 1/24/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var entryTitleTextField: UITextField!
+    @IBOutlet weak var entryTextView: UITextView!
+  
+}
---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Nathanael Youngren on 1/24/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    
+    
+    
+    // Properties
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var bodyText: UILabel!
+    
+}
