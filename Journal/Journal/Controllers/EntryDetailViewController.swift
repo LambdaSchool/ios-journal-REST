a8e64fc377877ffc2c9ae694bd9abb7cf2908119
Frulwinn <frulwinn@gmail.com>
2019-01-24 13:06:51 -0800

    added save button, textfield, and textview on EntryDetailVC and connected outlets

--- Journal/Journal/Controllers/EntryDetailViewController.swift
+++ Journal/Journal/Controllers/EntryDetailViewController.swift
@@ -4,6 +4,15 @@ import UIKit
 
 class EntryDetailViewController: UIViewController {
 
+    //MARK: Properties
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func save(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
