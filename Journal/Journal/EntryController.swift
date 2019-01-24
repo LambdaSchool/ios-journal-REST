b22d1beeb6338209a3d99c2cbd8170a4f23d1e5e
Frulwinn <frulwinn@gmail.com>
2019-01-24 13:21:02 -0800

    created EntryController file and baseURL property

---
+++ Journal/Journal/EntryController.swift
@@ -0,0 +1,10 @@
+//  Copyright © 2019 Frulwinn. All rights reserved.
+
+import Foundation
+
+let baseURL = URL(string: "https://books-118d9.firebaseio.com/")!
+
+class EntryController {
+    var entries: [Entry] = []
+    
+}
