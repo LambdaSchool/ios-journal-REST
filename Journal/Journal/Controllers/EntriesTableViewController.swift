88d228516f5cc443cff54a996d80b53a3a35f21b
Frulwinn <frulwinn@gmail.com>
2019-01-24 12:45:18 -0800

    created swift files for viewcontrollers and classed them on IB

---
+++ Journal/Journal/Controllers/EntriesTableViewController.swift
@@ -0,0 +1,43 @@
+//  Copyright © 2019 Frulwinn. All rights reserved.
+
+import UIKit
+
+class EntriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+     
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     
+     
+    }
+    */
+
+}
---
+++ Journal/Journal/Controllers/EntryDetailViewController.swift
@@ -0,0 +1,13 @@
+//  Copyright © 2019 Frulwinn. All rights reserved.
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
