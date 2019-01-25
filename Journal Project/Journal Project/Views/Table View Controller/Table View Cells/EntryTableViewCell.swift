bbc5ba2c59cd4110f5e2ca79ece55d1e6e1c838d
Michael R.F <mrflowers22@uwalumni.com>
2019-01-24 20:38:22 -0500

    entryTableViewCell is complete

--- Journal Project/Journal Project/Views/Table View Controller/Table View Cells/EntryTableViewCell.swift
+++ Journal Project/Journal Project/Views/Table View Controller/Table View Cells/EntryTableViewCell.swift
@@ -9,19 +9,23 @@
 import UIKit
 
 class EntryTableViewCell: UITableViewCell {
+    
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
+    
 
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var timestamp: UILabel!
     @IBOutlet weak var bodyText: UILabel!
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+ 
+    func updateViews(){
+        guard let passedInEntry = entry else { return }
+        title.text = passedInEntry.title
+        bodyText.text = passedInEntry.bodyText
+        timestamp.text = passedInEntry.timestamp.description
     }
 
 }
