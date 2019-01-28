97e450713769e66144d8d71d6208156f044d42e9
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-01-28 00:26:20 -0800

    complete storyboard layout, without polish

--- Journal - Firebase/Journal - Firebase/String Tracker.md
+++ Journal - Firebase/Journal - Firebase/String Tracker.md
@@ -4,4 +4,7 @@ EditEntrySegue
 
 https://lambdajournalentry.firebaseio.com/
 
+Missing from README:
 EntryTableCell
+Create an action from the add bar button item.
+
---
+++ Journal - Firebase/Journal - Firebase/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var save: UIBarButtonItem!
+    
+    
+}
+
