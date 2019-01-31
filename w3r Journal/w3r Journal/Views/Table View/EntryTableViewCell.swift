25c4e10ddf40a9518c42cad902ad4b2899c3495a
Michael R.F <mrflowers22@uwalumni.com>
2019-01-31 15:39:58 -0500

    structure the storyboard and organize files

---
+++ w3r Journal/w3r Journal/Views/Table View/EntryTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  EntryTableViewCell.swift
+//  w3r Journal
+//
+//  Created by Michael Flowers on 1/31/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var timestamp: UILabel!
+    @IBOutlet weak var bodyText: UILabel!
+    
+
+}
---
+++ w3r Journal/w3r Journal/Views/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  EntryDetailViewController.swift
+//  w3r Journal
+//
+//  Created by Michael Flowers on 1/31/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
